d7e89c97fe8344872fe4964b6d20ee368f16a008
VipexV4B <jeremytheriault23@gmail.com>
2026-04-28 16:43:48 -0400

    Beam

--- rm_messengers_rise/Assets/Script/joueur.cs
+++ rm_messengers_rise/Assets/Script/joueur.cs
@@ -5,63 +5,25 @@ using UnityEngine.Video;
 
 public class joueur : MonoBehaviour
 {
+    public GameObject Beam;
 
-    public GameObject ampoule;
-    public VideoPlayer videoPlayer;
-    public Animator animLumiere;
-    public int count;  
-    public HighScorePersistant pointage;
-    public Animator animCoin;
 
     void OnTriggerEnter(Collider other)
 {
-     if (other.CompareTag("sale_a_manger"))
+     if (other.CompareTag("Socket_1"))
     {
-        ampoule.SetActive(true);
+        Beam.SetActive(false);
     }
 
-     if (other.CompareTag("salon"))
-    {
-        videoPlayer.Play(); 
-    }
-
-     if (other.CompareTag("entree_maison"))
-    {
-        // Démarre l'animation
-        animLumiere.Play("fade_in");
-    }
-
-         if (other.CompareTag("coin"))
-    {
-        other.gameObject.SetActive(false);
-        pointage.OnChangerPointage(count++);
-    }
-
-         if (other.CompareTag("coin_fuite"))
-    {
-        // Démarre l'animation
-        animCoin.Play("etoile_fuite");
-    }
 }
 
 void OnTriggerExit(Collider other)
 {
 
-         if (other.CompareTag("sale_a_manger"))
+         if (other.CompareTag("Socket_1"))
     {
-        ampoule.SetActive(false);
+        Beam.SetActive(true);
     }
-
-         if (other.CompareTag("salon"))
-    {
-        videoPlayer.Stop(); 
-    }
-         if (other.CompareTag("entree_maison"))
-    {
-        // Démarre l'animation
-        animLumiere.Play("fade_out");
-    }
-
 }
 
 }
